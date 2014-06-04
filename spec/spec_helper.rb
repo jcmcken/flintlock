11074d65b32831ef14b93bdb31a450c6c2db3c1b
Jon McKenzie <jcmcken@gmail.com>
2014-06-04 00:17:39 -0400

    Add a stdout capturererer

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,14 @@
 require 'rspec'
 require 'flintlock'
 require 'fakefs/spec_helpers'
+
+def capture_stdout(&block)
+  orig_stdout = $stdout
+  $stoud = fake = StringIO.new
+  begin
+    yield
+  ensure
+    $stdout = orig_stdout
+  end
+  fake.string
+end
